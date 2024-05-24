3c068938f91d7d7f8d8685d4f1ef18d849f38a3e
hanssn <hans@team-xyz.com>
2024-05-24 14:59:17 +0700

    remove id

--- resources/lang/id/auth.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-return [
-    'failed' => 'Kredensial ini tidak cocok dengan catatan kami.',
-    'throttle' => 'Terlalu banyak percobaan login. Silakan coba lagi dalam :seconds detik.',
-];
